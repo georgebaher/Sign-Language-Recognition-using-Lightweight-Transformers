c25cbe5fd7aba18613aa91205420cfb539658264
George Boulos <george.boulos@uni-a.de>
2025-08-21 19:29:54 +0200

    Reset .gitignore and untrack ignored files

--- ViTPose/test_mmcv.py
+++
@@ -1,9 +0,0 @@
-import torch
-import mmcv
-from mmcv.ops import get_compiling_cuda_version, get_compiler_version
-
-print("--- Verification Successful ---")
-print(f"PyTorch version: {torch.__version__}")
-print(f"MMCV version: {mmcv.__version__}")
-print(f"MMCV CUDA version: {get_compiling_cuda_version()}")
-print(f"MMCV Compiler version: {get_compiler_version()}")
